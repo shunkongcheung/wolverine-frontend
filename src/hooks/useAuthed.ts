368f0ade904b68ea239223645aa57b11574835ff
Shun Kong Cheung <skcheung@amazon.com>
2021-12-22 10:13:37 -0800

    feat(useAuthed): accept isRedirect

--- src/hooks/useAuthed.ts
+++ src/hooks/useAuthed.ts
@@ -1,20 +1,35 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-function useAuthed() {
+interface AuthState {
+  loaded: boolean;
+  username: string;
+}
+
+function useAuthed(isRedirect = true) {
   // validate if user is logged in, if not, redirect to home page
   // a user is logged in if username exists in localStorage
   const router = useRouter();
 
-  const [username, setUsername] = useState("");
+  const [state, setState] = useState<AuthState>({
+    loaded: false,
+    username: "",
+  });
 
   useEffect(() => {
-    const username = localStorage.getItem("username");
-    if (!username) router.push("/");
-    setUsername(username as string);
-  }, [router, setUsername]);
+    const username = localStorage.getItem("username") as string;
+    setState({ loaded: true, username });
+  }, [router, setState]);
 
-  return username;
+  useEffect(() => {
+    // if not redirect, then that's it
+    if (!isRedirect) return;
+
+    // if loaded, and no username found, redirect to register page
+    if (state.loaded && !state.username) router.push("/register");
+  }, [isRedirect, state, router]);
+
+  return state.username;
 }
 
 export default useAuthed;
