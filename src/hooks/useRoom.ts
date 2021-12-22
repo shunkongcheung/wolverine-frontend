968bc892302527bb3f1f42de4ed827cf367943e0
Shun Kong Cheung <skcheung@amazon.com>
2021-12-22 12:28:52 -0800

    feat(useRoom): add total

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,5 @@
 import { doc, getFirestore, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getFirebaseApp } from "../utils";
 
 interface RoomState {
@@ -20,6 +20,13 @@ interface RoomState {
 function useRoom(roomId: string, initialValues: RoomState) {
   const [state, setState] = useState<RoomState>(initialValues);
 
+  const total = useMemo(() => {
+    const prophetCount = 1;
+    return (
+      state.wolf + state.wolfKing + state.witch + state.farmer + prophetCount
+    );
+  }, [state]);
+
   useEffect(() => {
     getFirebaseApp();
     const db = getFirestore();
@@ -29,7 +36,7 @@ function useRoom(roomId: string, initialValues: RoomState) {
     });
   }, [setState, roomId]);
 
-  return state;
+  return { ...state, total };
 }
 
 export default useRoom;
