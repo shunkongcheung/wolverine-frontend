2fc9d3a6e425c8f6dd9737102d5fff56c6b512a7
Shun Kong Cheung <skcheung@amazon.com>
2021-12-22 11:58:51 -0800

    feat(Button): init

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
+import Button from "./Button";
 import Card from "./Card";
 import Header from "./Header";
 import TabContainer from "./TabContainer";
 
-export { Card, Header, TabContainer };
+export { Button, Card, Header, TabContainer };
