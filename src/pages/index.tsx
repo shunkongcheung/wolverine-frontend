738b4e681da47e3a294b9c6c2c0ed80532b17edf
Shun Kong Cheung <skcheung@amazon.com>
2021-12-22 09:06:53 -0800

    feat(index): add link and Card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import styled from "styled-components";
+import { Card } from "../components";
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const Row = styled.div`
+  margin: 7px 0;
+`;
 
 const Home: NextPage = () => {
-  return <div>hello world</div>;
+  return (
+    <Container>
+      <Card>
+        <Content>
+          <Row>
+            <Link href="/rooms/create">
+              <a>創建房間 >></a>
+            </Link>
+          </Row>
+          <Row>
+            <Link href="/rooms/join">
+              <a>加入房間 >></a>
+            </Link>
+          </Row>
+        </Content>
+      </Card>
+    </Container>
+  );
 };
 
 export default Home;
