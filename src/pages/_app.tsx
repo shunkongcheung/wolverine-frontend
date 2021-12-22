2dfe967ac9f082acdd1a380364b3d72417c1f09f
Shun Kong Cheung <skcheung@amazon.com>
2021-12-22 01:04:05 -0800

    feat(index): testing firestore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+
+import { getFirebaseApp } from "../utils";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const run = async () => {
+      getFirebaseApp();
+      const db = getFirestore();
+      await setDoc(doc(db, "users", "testing1"), {
+        name: "testing1",
+      });
+    };
+
+    run();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
